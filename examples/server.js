aaf2735198c24fd553086d67c237ca203f00227d
chiuwingyan <775039533@qq.com>
2019-11-29 20:47:42 +0800

    处理请求body数据

--- examples/server.js
+++ examples/server.js
@@ -7,13 +7,31 @@ const config = require('./webpack.config')
 const app = new Koa()
 const compiler = webpack(config)
 const static = require('koa-static')
-const Router = require('koa-router');
+const Router = require('koa-router')
 
-let router = new Router();
-
-app.use(webpackDevMiddleware(compiler,{
-  publicPath: '/__build__/'
-}))
+let router = new Router()
+function parseData(ctx) {
+  return new Promise((resolve, reject) => {
+    try {
+      let postData = []
+      ctx.req.addListener('data', data => {
+        // 有数据传入的时候
+        postData.push(data)
+      })
+      ctx.req.on('end', () => {
+        let parseData = Buffer.concat(postData)
+        resolve(parseData)
+      })
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: '/__build__/'
+  })
+)
 
 app.use(webpackHotMiddleware(compiler))
 
@@ -22,16 +40,27 @@ app.use(static(__dirname))
 app.use(bodyParser())
 
 router.get('/simple/get', async ctx => {
-    ctx.body = {
-      msg: 'hello world'
-    }
-});
-router.get('/base/get',async ctx => {
+  ctx.body = {
+    msg: 'hello world'
+  }
+})
+router.get('/base/get', async ctx => {
   let ctx_query = ctx.query
   ctx.body = {
     ctx_query
   }
 })
+
+router.post('/base/buffer', async (ctx, next) => {
+          let postData = await parseData(ctx)
+          ctx.body = postData
+})
+
+router.post('/base/post', async ctx => {
+  let ctx_body = ctx.request.body
+  ctx.body = ctx_body
+})
+
 app.use(router.routes())
 
 const port = process.env.PORT || 8888
--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -4,6 +4,12 @@ export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
-export function isObject(val: any): val is Object {
-  return val !== null && typeof val === 'object'
+// 这个方法对于FormData、ArrayBuffer这些类型也判断为true
+// export function isObject(val: any): val is Object {
+//   return val !== null && typeof val === 'object'
+// }
+
+//这个方法只对json对象才能满足
+export function isPlainObject(val: any): val is Object {
+  return toString.call(val) === '[object Object]'
 }
